76d0dd8de83fbfee28d782801e9200edb9641ba1
August Duet <august.duet@revature.com>
2019-11-08 00:16:00 -0500

    last effect of the transfer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
-export class AppRoutingModule { }
-=======
 export class AppRoutingModule {}
->>>>>>> 6dae3aed52d7a9ecc112abbc5690c4b5220e17c3
